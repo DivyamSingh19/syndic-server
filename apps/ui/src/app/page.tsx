c29b0177a3ef5e7bb6b2f55fc388a64b218afa96
Divyam Singh <divyamsingh1918@gmail.com>
2025-09-27 14:46:42 +0530

    razor-pay-test

---
+++ apps/ui/src/app/page.tsx
@@ -0,0 +1,12 @@
+import PaymentButton from '@/components/paymentButton'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <PaymentButton amount={10}/>
+    </div>
+  )
+}
+
+export default page
